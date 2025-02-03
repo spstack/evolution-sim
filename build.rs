10ac41f46fb29004aeb4f087c65d76ba1596635a
Scott Stack <scottstack14@gmail.com>
2025-02-03 00:46:42 -0500

    Fix build.rs that was accidentally overwritten

--- build.rs
+++ build.rs
@@ -3,27 +3,27 @@
  * 
  * This is used to build/link the required external C driver for the matrix LED panels
  */
-fn main() {}
 
-// fn main() {
-//     // Command to cargo to tell it to link the librgbmatrix library statically
-//     // println!("cargo::rustc-link-search=/usr/lib/librgbmatrix");
-//     // println!("cargo::rustc-link-lib=static=rgbmatrix");
-//     cc::Build::new()
-//         .cpp(true)
-//         .includes(["src/rpi-rgb-led-matrix/include"])
-//         .file("src/rpi-rgb-led-matrix/lib/bdf-font.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/content-streamer.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/framebuffer.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/gpio.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/hardware-mapping.c")
-//         .file("src/rpi-rgb-led-matrix/lib/led-matrix.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/pixel-mapper.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/graphics.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/led-matrix-c.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/multiplex-mappers.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/options-initialize.cc")
-//         .file("src/rpi-rgb-led-matrix/lib/thread.cc")
-//         .cpp_link_stdlib("stdc++")
-//         .compile("rgbmatrix");
-// }
+
+fn main() {
+    // Command to cargo to tell it to link the librgbmatrix library statically
+    // println!("cargo::rustc-link-search=/usr/lib/librgbmatrix");
+    // println!("cargo::rustc-link-lib=static=rgbmatrix");
+    cc::Build::new()
+        .cpp(true)
+        .includes(["src/rpi-rgb-led-matrix/include"])
+        .file("src/rpi-rgb-led-matrix/lib/bdf-font.cc")
+        .file("src/rpi-rgb-led-matrix/lib/content-streamer.cc")
+        .file("src/rpi-rgb-led-matrix/lib/framebuffer.cc")
+        .file("src/rpi-rgb-led-matrix/lib/gpio.cc")
+        .file("src/rpi-rgb-led-matrix/lib/hardware-mapping.c")
+        .file("src/rpi-rgb-led-matrix/lib/led-matrix.cc")
+        .file("src/rpi-rgb-led-matrix/lib/pixel-mapper.cc")
+        .file("src/rpi-rgb-led-matrix/lib/graphics.cc")
+        .file("src/rpi-rgb-led-matrix/lib/led-matrix-c.cc")
+        .file("src/rpi-rgb-led-matrix/lib/multiplex-mappers.cc")
+        .file("src/rpi-rgb-led-matrix/lib/options-initialize.cc")
+        .file("src/rpi-rgb-led-matrix/lib/thread.cc")
+        .cpp_link_stdlib("stdc++")
+        .compile("rgbmatrix");
+}
